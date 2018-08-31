b3f47163b3d50eac10b37a3ab7f00e05e9672213
sophie19921005 <m18768120089@163.com>
2018-08-31 21:30:19 +0800

    challenge5_1

--- spider.py
+++ spider.py
@@ -1,16 +1,13 @@
-
 import json
-
 from selenium import webdriver
 from scrapy.http import HtmlResponse
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-results = []
+results =[]
 def parse(response):
     for comment in response.css('div.comment-list-item'):
-        results = []
         result = {}
         result['comments'] = comment.css('div.comment-item-content p::text').extract_first()
         result['username']=comment.css('div.user-username a::text').extract_first().strip()
@@ -23,31 +20,37 @@ def has_next_page(response):
     return next_page
 
 def goto_next_page(driver):
-    pass
+    driver.find_element_by_xpath("//a[contains(text(),'下一页')]").click()
 
 def wait_page_return(driver,page):
-    WebDriverWait(driver,10).unyil(
-            EC.text_to_be_present_in_element(
-               (By.XPATH,'//ul[@class="pagination"]/li[@class="active"]'),
+    WebDriverWait(driver,10).until(
+        EC.text_to_be_present_in_element(
+            (By.XPATH, '//ul[@class="pagination"]/li[@class="active"]'),
             str(page)
-            )
-            )
+        )
+    )
+
 def spider():
     driver = webdriver.PhantomJS()
-    url = '//ul[@class="pagination"]/li[@class="active"]'
+    url = 'https://www.shiyanlou.com/courses/427'
     driver.get(url)
     page=1
     while True:
         wait_page_return(driver,page)
         html = driver.page_source
-        response=HtmlResponse(url=url.body=html.encode('utf8'))
+        response=HtmlResponse(url=url,body=html.encode('utf8'))
         parse(response)
+        '''
         if not has_next_page(response):
             break
+        '''
+        if response.css('li.disabled.next-page'):
+            break
         page+=1
+        print(page)
         goto_next_page(driver)
     with open('/home/shiyanlou/comments.json','w') as f:
-        f.write(json.dums(results))
+        json.dump(results, f)
 
 if __name__=='__main__':
     spider()
